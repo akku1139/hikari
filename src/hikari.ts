6bc097472463c13d8c61458470d81627fbf9935a
-akku- <151517253+akku1139@users.noreply.github.com>
2025-02-17 18:20:40 +0900

    improve method routes

--- src/hikari.ts
+++ src/hikari.ts
@@ -1,3 +1,4 @@
+import type { METHODS } from "./define"
 import { HikariCore } from "./hikari-core"
 import type { Handler } from "./types"
 
@@ -6,22 +7,23 @@ export class Hikari extends HikariCore {
     super()
   }
 
-  get(path: string, ...handlers: Array<Handler>) {
-    return this.on("GET", path, handlers)
+  #methodRoute(method: typeof METHODS[number]) {
+    return (path: string, ...handlers: Array<Handler>) => this.on(method, path, handlers)
   }
-  post(path: string, ...handlers: Array<Handler>) {
-    return this.on("POST", path, handlers)
+  get = this.#methodRoute("GET")
+  post = this.#methodRoute("POST")
+  put = this.#methodRoute("PUT")
+  delete = this.#methodRoute("DELETE")
+  // options = this.#methodRoute("OPTIONS")
+  patch = this.#methodRoute("PATCH")
+  use(path: string, ...handlers: Array<Handler>) {
+    return this
+      .on("GET", path, handlers)
+      .on("POST", path, handlers)
+      .on("PUT", path, handlers)
+      .on("DELETE", path, handlers)
+      .on("OPTIONS", path, handlers)
+      .on("PATCH", path, handlers)
   }
-  put(path: string, ...handlers: Array<Handler>) {
-    return this.on("PUT", path, handlers)
-  }
-  delete(path: string, ...handlers: Array<Handler>) {
-    return this.on("DELETE", path, handlers)
-  }
-  // options(path: string, ...handlers: Array<Handler>) {
-  //   return this.on("OPTIONS", path, handlers)
-  // }
-  patch(path: string, ...handlers: Array<Handler>) {
-    return this.on("PATCH", path, handlers)
-  }
+  all = this.use
 }
