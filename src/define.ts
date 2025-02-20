10e089f20781d27f3d8631de4267bcc42c593527
-akku- <151517253+akku1139@users.noreply.github.com>
2025-02-20 18:59:28 +0900

    test

--- src/define.ts
+++ src/define.ts
@@ -1,1 +1,2 @@
 export const METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"] as const
+export const METHODS_LOWERCASE = ["get", "post", "put", "delete", "options", "patch"] as const
--- src/hikari.test.ts
+++ src/hikari.test.ts
@@ -1,6 +1,18 @@
+import { METHODS_LOWERCASE } from "./define.ts"
 import { Hikari } from "./hikari.ts"
 import test from "node:test"
+import { strictEqual } from "node:assert/strict"
 
 test("new Hikari()", () => {
   new Hikari()
+})
+
+test("HTTP methods", async (t) => {
+  for(const method of METHODS_LOWERCASE) {
+    await t.test(method, async () => {
+      const app = new Hikari()[method]("/", () => new Response("Res"))
+      const res = await app.fetch(new Request("https://example.com/", { method: method.toUpperCase() }))
+      strictEqual(await res.text(), "Res")
+    })
+  }
 })
