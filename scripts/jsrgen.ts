f62f48a3697a78792fdfab1760abb44ddc53634e
-akku- <151517253+akku1139@users.noreply.github.com>
2025-02-23 01:52:27 +0900

    JSR 10

--- scripts/jsrgen.ts
+++ scripts/jsrgen.ts
@@ -2,33 +2,35 @@ import * as process from "node:process"
 import * as fs from "node:fs"
 import * as path from "node:path"
 
-import * as pkg from "../package.json"
+import * as pkg from "../package.json" with { type: "json" }
 
 if(process.env.npm_package_version === void 0) {
   throw new Error("process.env.npm_package_version is undefined.")
 }
 
+const jsrjson = {
+  name: "@hikarijs/hikari",
+  version: pkg.version,
+  license: pkg.license,
+  exports: Object.fromEntries(Object.entries(pkg.exports).map(v => [
+    v[0], v[1].import.replace(/^\.\/dist\//, "./src/").replace(/\.js$/, ".ts")
+  ])),
+  publish: {
+    include: [
+      "jsr.jsonc",
+      "LICENSE",
+      "README.md",
+      "src/**/*.ts"
+    ],
+    exclude: [
+      "src/**/*.test.ts"
+    ]
+  }
+} as const
+
+console.log("Generated jsr.json", JSON.stringify(jsrjson, null, 2))
+
 fs.writeFileSync(
   path.join(process.cwd(), "jsr.json"),
-  JSON.stringify(
-    {
-      name: "@hikarijs/hikari",
-      version: pkg.version,
-      license: pkg.license,
-      exports: Object.fromEntries(Object.entries(pkg.exports).map(v => [
-        v[0], v[1].import.replace(/^\.\/dist\//, "./src/").replace(/\.js$/, ".ts")
-      ])),
-      publish: {
-        include: [
-          "jsr.jsonc",
-          "LICENSE",
-          "README.md",
-          "src/**/*.ts"
-        ],
-        exclude: [
-          "src/**/*.test.ts"
-        ]
-      }
-    }
-    )
+  JSON.stringify(jsrjson)
 )
